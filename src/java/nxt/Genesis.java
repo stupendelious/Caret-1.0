57613baa2f5850fb0bf364491dc31cd1cf26290d
stupendelious <45348932+stupendelious@users.noreply.github.com>
2018-12-04 16:28:54 -0600

    Update Genesis.java

--- src/java/nxt/Genesis.java
+++ src/java/nxt/Genesis.java
@@ -7,8 +7,8 @@ public final class Genesis {
     public static final long GENESIS_BLOCK_ID = 1528037899032245112L;
     public static final long CREATOR_ID = 0L;
     public static final byte[] CREATOR_PUBLIC_KEY = {
-    	1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-    	0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0
+    	1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
+    	0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0
     };
 
     public static final long[] GENESIS_RECIPIENTS = {};
